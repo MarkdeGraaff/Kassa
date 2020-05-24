f946a615892d4ca60ba9193303623087669fdcaa
MarkdeGraaff <d275386@edu.rocwb.nl>
2020-05-24 11:49:59 +0200

    Database en plaatjes

--- Kassasys/Kassasys/Properties/Settings.Designer.cs
+++ Kassasys/Kassasys/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Kassasys.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace Kassasys.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-4378TN4;Initial Catalog=dbkassa;Integrated Security=True;Conn" +
-            "ect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=dbkassa;Integrated Security=True;Connect Timeout=30" +
+            ";Encrypt=False;TrustServerCertificate=False")]
         public string dbkassaConnectionString {
             get {
                 return ((string)(this["dbkassaConnectionString"]));
